9c54e76155055faa902ff35f3d9822bd809fafdf
Jacob Marshall <jacob.marshall.jem@gmail.com>
2021-02-17 15:38:11 +0000

    Random stuff

--- form/FormController.js
+++ form/FormController.js
@@ -5,4 +5,22 @@ var bodyParser = require('body-parser');
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 var Form = require('./form');
+router.post('/', function (req, res) {
+    Form.create({
+            name : req.body.name,
+            email : req.body.email,
+            company : req.body.company
+        }, 
+        function (err, form) {
+            if (err) return res.status(500).send("There was a problem adding the information to the database.");
+            res.status(200).send(form);
+        });
+});
+// RETURNS ALL THE USERS IN THE DATABASE
+router.get('/', function (req, res) {
+    Form.find({}, function (err, form) {
+        if (err) return res.status(500).send("There was a problem finding the users.");
+        res.status(200).send(form);
+    });
+});
 module.exports = router;
